6a30f8591055a9b045076178691081d989188cf4
Daniil <danil_semenov18@mail.ru>
2025-01-31 13:52:19 +0300

    Escaping fixs

--- deployNginxWebsite.sh
+++ deployNginxWebsite.sh
@@ -34,7 +34,7 @@ server {
     index index.html index.htm index.php;
 
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files \\\$uri \\\$uri/ =404;
     }
 
     location ~ \.php$ {
